4c467c5de433ddd3a16e73c0ad8f7190bf54d169
Nurlan622 <7bxwn9m@code.edu.az>
2023-04-25 00:57:18 +0400

    function

---
+++ assets/js/main.js
@@ -0,0 +1,53 @@
+// ------------------------------------Task a--------------------------------------------------------
+// function calc(a, b, operator) {  
+//     switch (operator) {
+//         case "+":
+//             return a + b;
+//         case "-":
+//             return a - b;
+//         case "*":
+//             return a * b;
+//         case "/":
+//             return a / b;
+//         default:
+//             return "Yanlis emeliyyat"
+//     }    
+// }
+// alert(calc(Number(prompt("birinci eded")), Number(prompt("ikinci eded")), prompt("operator")));
+// ------------------------------------Task b--------------------------------------------------------
+
+function surName(name, surname, fathername) {
+
+}
+
+// ------------------------------------Task c--------------------------------------------------------
+
+
+// function divideSay(num) {
+//     var s = 0;
+//     if (num > 0 && num < 50) {
+//         for (let i = 1; i < num; i++) {
+//             if (i % 3 === 0) {
+//                 s++;
+//             }
+//         }
+//     }
+//     else if (num > 50 && num < 100){
+//         for (let i = 1; i < num; i++) {
+//             if (i % 5 === 0) {
+//                 s++;
+//             }
+//         }
+//     }
+//     else{
+//         for (let i = 1; i < num; i++) {
+//             if (i % 8 === 0) {
+//                 s++;
+//             }
+//         }
+//     }
+//     return s;
+// }
+// alert(divideSay(Number(prompt("ededi daxil edin"))));
+
+
